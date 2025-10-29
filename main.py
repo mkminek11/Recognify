2a03f51b0606bcf75aeaba3c152166d22952d7c3
mkminek11 <mkminek11@seznam.cz>
2025-10-29 13:16:14 +0100

    Production fix

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from app.lib import app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     host = "0.0.0.0" if os.environ.get("RENDER") else "127.0.0.1"
-    debug = os.environ.get("FLASK_ENV") == "development"
+    debug = not os.environ.get("RENDER") and os.environ.get("FLASK_ENV") == "development"
     
     app.run(host=host, port=port, debug=debug)
